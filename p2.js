c9f1ab24c16fc6c0ee0693e580c273bfe401111a
LakshmiBhavaniC <166042151+LakshmiBhavaniC@users.noreply.github.com>
2024-04-05 15:48:16 +0530

    Updated p2.js

--- p2.js
+++ p2.js
@@ -1,7 +1,12 @@
 let obj = {
 name : "Bhavani",
 age : 23,
-"place :"Hyderabad",
+place : "Hyderabad",
 }
 
-console.log(obj)
+let obj2 = {
+  ...obj,
+  hobbies : "Cycling",
+  blood_group : "AB+VE",
+}
+console.log(obj2)
